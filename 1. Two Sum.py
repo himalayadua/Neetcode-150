a05b8dd94994bcbbfafa16b8214a6ec20e2d18cd
himalayadua <67844234+himalayadua@users.noreply.github.com>
2024-05-17 17:02:44 +0000

    may17 c4

---
+++ 1. Two Sum.py
@@ -0,0 +1,16 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]: # type: ignore
+        #hash table to store value and index
+        number_map = {}
+
+        #store each element and index in the hash table
+        for i in range(len(nums)):
+            number_map[nums[i]] = i
+
+        #check if the complement of each element exists in the hash table
+        for i in range(len(nums)):
+            complement = target - nums[i]
+
+            #if it does -> that's our answer
+            if complement in number_map and number_map[complement] != i:
+                return [i, number_map[complement]]
