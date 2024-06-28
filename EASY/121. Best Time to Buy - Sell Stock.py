2e68ddd21b679df0cced42b9f45167cb4d798485
Himalaya Dua <67844234+himalayadua@users.noreply.github.com>
2024-06-28 17:57:18 +0530

    jun28 c1

---
+++ EASY/121. Best Time to Buy - Sell Stock.py
@@ -0,0 +1,14 @@
+from typing import List
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        maxProfit = 0
+        buy,sell =0,1
+    
+        while sell < len(prices):
+            maxProfit = max(maxProfit, prices[sell] - prices[buy])
+            if prices[sell] < prices[buy]: buy = sell
+            sell += 1
+            
+        return maxProfit
