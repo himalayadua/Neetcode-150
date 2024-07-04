ce91535167dfb2068a76c175a1df04f81cc22b12
Himalaya Dua <67844234+himalayadua@users.noreply.github.com>
2024-07-04 21:22:32 +0530

    jul04 c4

--- EASY/104. Maximum Depth of Binary Tree.py
+++ EASY/104. Maximum Depth of Binary Tree.py
@@ -25,3 +25,10 @@ class Solution:
             return max(dfs(root.left, depth + 1), dfs(root.right, depth + 1))
                        
         return dfs(root, 0)
+
+# Approach 3
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        return 1 + max(self.maxDepth(root.right), self.maxDepth(root.left))
