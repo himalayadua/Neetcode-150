ec747ecc38b7b904e91af61a8f88bd02e2592153
Himalaya Dua <67844234+himalayadua@users.noreply.github.com>
2024-07-04 21:13:02 +0530

    jul04 c3

--- EASY/104. Maximum Depth of Binary Tree.py
+++ EASY/104. Maximum Depth of Binary Tree.py
@@ -6,7 +6,7 @@ class TreeNode:
         self.right = right
 from typing import Optional
 
-
+# Approach 1
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
         if root == None:
@@ -15,3 +15,13 @@ class Solution:
         left_depth = self.maxDepth(root.left)
         right_depth = self.maxDepth(root.right)
         return 1 + max(left_depth, right_depth)
+    
+
+# Approach 2
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        def dfs(root, depth):
+            if not root: return depth
+            return max(dfs(root.left, depth + 1), dfs(root.right, depth + 1))
+                       
+        return dfs(root, 0)
