4250905d397607f8f408f01a7f5c7f35a294ba1d
Himalaya Dua <67844234+himalayadua@users.noreply.github.com>
2024-10-08 14:44:35 -0700

    oct8th lvl3

--- Online Assessments/inMemoryDB.py
+++ Online Assessments/inMemoryDB.py
@@ -45,4 +45,21 @@ class inMemoryDB:
             # <expression> for-loop <variable-n> in [list]{dict} if <condition>
             return strn
 
-    
+
+    def scan_by_prefix(self, key: str, prefix: str) -> list[str]:
+        if key not in self.database:
+            return []
+        else:
+            strn = [f"{field}({value})" for field, value in sorted(self.database[key].items()) if field.startswith(prefix)]
+            # list comprehension
+            
+            return strn
+
+# Level 3
+# SET_AT <key> <field> <value> <timestamp> — should insert a field-value pair or updates the value of the field in the record associated with key. This operation should return an empty string.
+# SET_AT_WITH_TTL <key> <field> <value> <timestamp> <ttl> — should insert a field-value pair or update the value of the field in the record associated with key. Also sets its Time-To-Live starting at timestamp to be ttl. The ttl is the amount of time that this field-value pair should exist in the database, meaning it will be available during this interval: [timestamp, timestamp + ttl). This operation should return an empty string.
+# DELETE_AT <key> <field> <timestamp> — the same as DELETE, but with timestamp of the operation specified. Should return "true" if the field existed and was successfully deleted and "false" if the key didn't exist.
+# GET_AT <key> <field> <timestamp> — the same as GET, but with timestamp of the operation specified.
+# SCAN_AT <key> <timestamp> — the same as SCAN, but with timestamp of the operation specified.
+# SCAN_BY_PREFIX_AT <key> <prefix> <timestamp> — the same as SCAN_BY_PREFIX, but with timestamp of the operation specified.
+
